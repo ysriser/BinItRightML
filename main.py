f26e67e1e8fdcc985f40e35d6fa73e09520aa7c5
Yue-Sheng <yuesheng1995@gmail.com>
2026-01-25 17:32:44 +0800

    fix lint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
 app = FastAPI()
 
 
@@ -12,7 +12,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
 
 @app.middleware("http")
 async def add_security_headers(request, call_next):

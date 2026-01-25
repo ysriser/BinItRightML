aabbe54ad4e09e58cfe426c945ca367b4ceb4073
Yue-Sheng <yuesheng1995@gmail.com>
2026-01-25 17:31:17 +0800

    fix warnings on DAST

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Response
 app = FastAPI()
 
 
@@ -13,3 +12,13 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.middleware("http")
+async def add_security_headers(request, call_next):
+    response = await call_next(request)
+    # Fix for Alert 10021
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    # Fix for Alert 90004
+    response.headers["Cross-Origin-Resource-Policy"] = "same-origin"
+    return response

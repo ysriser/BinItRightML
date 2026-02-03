38dca6f4b49c8ef0e9a46a0af9ae86fccb6b4843
e1616033 <150251429+lachupriya0993@users.noreply.github.com>
2026-02-03 08:51:38 +0800

    Implement forecast endpoint with data from pickle file
    Added a new endpoint to retrieve forecasts based on loaded data.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Union
+import pickle
 
 from fastapi import FastAPI
 app = FastAPI()
@@ -12,6 +13,29 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+with open("forecasts1.pkl", "rb") as f:
+    BEST_FORECASTS = pickle.load(f)
+
+@app.get("/forecast")
+def get_final_forecast():
+
+    years = list(range(2025, 2031))
+
+    response = {
+        "forecasts": {},
+        "calculated_total_generated_tonnes": round(
+            BEST_FORECASTS["Total_tonnes"], 2
+        )
+    }
+
+    for model_name in ["ARIMA", "WMA", "SES"]:
+        response["forecasts"][model_name] = dict(
+            zip(years, map(int, BEST_FORECASTS[model_name]))
+        )
+
+    return response
+
 
 @app.middleware("http")
 async def add_security_headers(request, call_next):
